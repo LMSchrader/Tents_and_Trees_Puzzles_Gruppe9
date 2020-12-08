aeaddb36068ad5e165dea687e224c182ea77eb5e
Lisa <lisa-maria.schrader@uni-oldenburg.de>
2020-12-08 20:44:44 +0100

    initial commit

---
+++ Tents_and_Trees_Puzzles/src/Puzzle.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Puzzle {
+	private String[][] puzzle;
+	
+	public Puzzle(String fileName) {
+		setPuzzle(fileName);
+	}
+	
+	public void setPuzzle(String fileName) {
+		List<String[]> p = readFromCsvFile(",", fileName);
+		
+		this.puzzle = new String[p.size()][p.get(0).length];
+		
+		for (int i = 0; i < p.size(); i++) {
+			this.puzzle[i] = p.get(i);
+		}
+	}
+	
+	private List<String[]> readFromCsvFile(String separator, String fileName) {
+	    try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+	        List<String[]> list = new ArrayList<>();
+	        String line = "";
+	        while((line = reader.readLine()) != null){
+	            String[] array = line.split(separator, -1);
+	            list.add(array);
+	        }
+	        return list;
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	        return null;
+	    }  
+	}
+
+}
---
+++ Tents_and_Trees_Puzzles/src/Test.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Puzzle p = new Puzzle("src" + File.separatorChar + "resources" + File.separatorChar + "tents_trees_0.csv");
+
+	}
+
+}
