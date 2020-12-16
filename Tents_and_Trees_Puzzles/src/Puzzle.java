81abb4362b6e37ea23079d2600addeac561773ae
Lisa <Lisa@DESKTOP-EU2BH49>
2020-12-16 18:49:42 +0100

    Added preprocessing steps (4+5)

--- Tents_and_Trees_Puzzles/src/Puzzle.java
+++ Tents_and_Trees_Puzzles/src/Puzzle.java
@@ -5,10 +5,101 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Puzzle {
+	// representation:
+	// tree: "t"
+	// tent: "^"
+	// gras: "g"
+	// unknown: ""
 	private String[][] puzzle;
+	
 	
 	public Puzzle(String fileName) {
 		setPuzzle(fileName);
+	}
+	
+	
+	public int countNumberOfXInRow(int row, String x) {
+		if (isLegalRowIndex(row)) {
+			int count = 0;
+			for (int i = 1; i < getColumns(); i++) {
+				if (x.equals(this.puzzle[row][i])) {
+					count++;
+				}
+			}
+			return count;
+		} else {
+			throw new IndexOutOfBoundsException("Index " + row + " is out of bounds!");
+		}
+	}
+	
+	public int countNumberOfXInColumn(int column, String x) {
+		if (isLegalColumnIndex(column)) {
+			int count = 0;
+			for (int i = 1; i < getRows(); i++) {
+				if (x.equals(this.puzzle[i][column])) {
+					count++;
+				}
+			}
+			return count;
+		} else {
+			throw new IndexOutOfBoundsException("Index " + column + " is out of bounds!");
+		}
+	}
+	
+	public int numberOfTentsThatShouldBeInRow(int row) {
+		if (isLegalRowIndex(row)) {
+			return Integer.parseInt(this.puzzle[row][0]);
+		} else {
+			throw new IndexOutOfBoundsException("Index " + row + " is out of bounds!");
+		}
+	}
+	
+	public int numberOfTentsThatShouldBeInCollumn(int column) {
+		if (isLegalColumnIndex(column)) {
+			return Integer.parseInt(this.puzzle[0][column]);
+		} else {
+			throw new IndexOutOfBoundsException("Index " + column + " is out of bounds!");
+		}
+	}
+	
+	public void fillUnknownFieldsofRowWithX(int row, String x) {
+		if (isLegalRowIndex(row)) {
+			for (int i = 1; i < getColumns(); i++) {
+				if (this.puzzle[row][i].equals("")) {
+					this.puzzle[row][i] = x;
+				}
+			}
+		} else {
+			throw new IndexOutOfBoundsException("Index " + row + " is out of bounds!");
+		}
+	}
+	
+	public void fillUnknownFieldsofColumnWithX(int column, String x) {
+		if (isLegalColumnIndex(column)) {
+			for (int i = 1; i < getRows(); i++) {
+				if (this.puzzle[i][column].equals("")) {
+					this.puzzle[i][column] = x; 
+				}
+			}
+		} else {
+			throw new IndexOutOfBoundsException("Index " + column + " is out of bounds!");
+		}
+	}
+	
+	public int getRows() {
+		return this.puzzle.length;
+	}
+	
+	public int getColumns() {
+		return this.puzzle[0].length;
+	}
+	
+	private boolean isLegalRowIndex(int row) {
+		return row < getRows() && row > 0;
+	}
+	
+	private boolean isLegalColumnIndex(int column) {
+		return column < getColumns() && column > 0;
 	}
 	
 	public void setPuzzle(String fileName) {
@@ -19,6 +110,10 @@ public class Puzzle {
 		for (int i = 0; i < p.size(); i++) {
 			this.puzzle[i] = p.get(i);
 		}
+	}
+	
+	public void setPuzzle(String[][] puzzle) {
+		this.puzzle = puzzle;
 	}
 	
 	// source: https://riptutorial.com/csv/example/27605/reading-and-writing-in-java
@@ -37,4 +132,7 @@ public class Puzzle {
 	    }  
 	}
 
+	public String[][] getPuzzle() {
+		return this.puzzle;
+	}
 }
--- Tents_and_Trees_Puzzles/src/Test.java
+++ Tents_and_Trees_Puzzles/src/Test.java
@@ -1,10 +1,32 @@
 import java.io.File;
 
 public class Test {
+	private static Puzzle puzzle;
 
 	public static void main(String[] args) {
-		Puzzle p = new Puzzle("src" + File.separatorChar + "resources" + File.separatorChar + "tents_trees_0.csv");
+		puzzle = new Puzzle("src" + File.separatorChar + "resources" + File.separatorChar + "tents_trees_0.csv");
 
+		preprocessing3();
 	}
 
+	/**
+	 * Wenn in einer Zeile oder Spalte nur noch so viele moegliche Felder gibt, um Zelte aufstellen zu koennen, 
+	 * wie Zelte in der Zeile oder Spalte stehen koennen, kann direkt ein Zelt gesetzt werden. 
+	 */
+	public static void preprocessing3() {
+		for (int i = 1; i < puzzle.getRows(); i++) {
+			int numberOfMinssingTents = puzzle.numberOfTentsThatShouldBeInRow(i) - puzzle.countNumberOfXInRow(i, "^");
+			if (puzzle.countNumberOfXInRow(i, "") == numberOfMinssingTents) {
+				puzzle.fillUnknownFieldsofRowWithX(i, "^");
+			}
+		}
+		
+		for (int i = 1; i < puzzle.getColumns(); i++) {
+			int numberOfMinssingTents = puzzle.numberOfTentsThatShouldBeInCollumn(i) - puzzle.countNumberOfXInColumn(i, "^");
+			if (puzzle.countNumberOfXInColumn(i, "") == numberOfMinssingTents) {
+				puzzle.fillUnknownFieldsofColumnWithX(i, "^");
+			}
+		}
+	}
+	
 }
