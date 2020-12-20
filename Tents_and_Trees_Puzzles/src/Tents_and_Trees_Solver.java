00641223560f5896720c57c461c882e1a08768d2
Lisa <Lisa@DESKTOP-EU2BH49>
2020-12-20 14:02:26 +0100

    puzzle not needed

--- Tents_and_Trees_Puzzles/src/Tents_and_Trees_Solver.java
+++ Tents_and_Trees_Puzzles/src/Tents_and_Trees_Solver.java
@@ -1,14 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class Tents_and_Trees_Solver {
-	private Puzzle p;
+	private String[][] puzzle;
 	
 	private Stack<Node> currentPath = new Stack<>(); // stack for backtrack
 	private Node currentNode;
 	
 	
 	public Tents_and_Trees_Solver(String fileName) {
-		p = new Puzzle(fileName);
+		setPuzzle(fileName);
 		csp();
 	}
 	
@@ -26,4 +31,41 @@ public class Tents_and_Trees_Solver {
 //		ENDIF
 //		END WHILE
 	}
+	
+	
+	public void setPuzzle(String fileName) {
+		List<String[]> p = readFromCsvFile(",", fileName);
+		
+		this.puzzle = new String[p.size()][p.get(0).length];
+		
+		for (int i = 0; i < p.size(); i++) {
+			this.puzzle[i] = p.get(i);
+		}
+	}
+	
+	// source: https://riptutorial.com/csv/example/27605/reading-and-writing-in-java
+	private List<String[]> readFromCsvFile(String separator, String fileName) {
+	    try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+	        List<String[]> list = new ArrayList<>();
+	        String line = "";
+	        while((line = reader.readLine()) != null){
+	            String[] array = line.split(separator, -1);
+	            list.add(array);
+	        }
+	        return list;
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	        return null;
+	    }  
+	}
+	
+	public void printPuzzle() {
+		for (int i = 0; i < puzzle.length; i++) {
+			for (int j = 0; j < puzzle[i].length; j++) {
+				System.out.print(puzzle[i][j] + "\t");
+			}
+			System.out.println("\n\n");
+		}
+	}
+
 }
